1f19028ea8bcd34c3929048e652b6edcbbb75481
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-07 13:14:44 +0530

    Auth Routes added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Home } from './pages/Home'
-
+import { Signin } from './pages/Signin'
+import { Signup } from './pages/Signup'
 function App() {
   
 
@@ -10,6 +11,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
     </BrowserRouter>
   )
