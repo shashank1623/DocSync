61b29f55e3df05115b627ee9eb73497c6351d801
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-08 10:19:07 +0530

    Module used by both frontend and backend

---
+++ common/src/index.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const signinInput = z.object({
+    email : z.string().email(),
+    password : z.string().min(6)
+})
+
+
+export type signinInput = z.infer<typeof signinInput>
