247bccf591207b4ee088adc1e71e1c0f6651420c
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-15 13:07:34 +0530

    Added Dashboard Router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRouter from './routes/user';
+import dashboardRouter from './routes/dashboard';
 import cors from 'cors'
 const app = express();
 const port = 3000;
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 app.use("/api/v1/user",userRouter);
+app.use("/api/v1/dashboard",dashboardRouter);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
