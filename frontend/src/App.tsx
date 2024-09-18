a23be34ea1b0117644ca3a2d6a857c2a291aaeff
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-18 10:35:55 +0530

    Updated Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Home } from './pages/Home';
+import { Signin } from './pages/Signin';
+import { Signup } from './pages/Signup';
+import { Dashboard } from './pages/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+import DocSyncEditor from './components/DocSyncEditor';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import { Home } from './pages/Home'
-import { Signin } from './pages/Signin'
-import { Signup } from './pages/Signup'
-import { Dashboard } from './pages/Dashboard'
-import ProtectedRoute from './components/ProtectedRoute'
-import DocSyncEditor from './components/DocSyncEditor'
 function App() {
-
-
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public Routes */}
         <Route path='/' element={<Home />} />
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
 
+        {/* Protected Routes */}
         <Route element={<ProtectedRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
+          {/* Document Editor Route with id and accessType as parameters */}
           <Route path='/dashboard/document/d/:id/:accessType' element={<DocSyncEditor />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-
-export default App
+export default App;
