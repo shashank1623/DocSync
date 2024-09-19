8823a1ba414c3c33e2b833c4b5143edc5b70450f
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-19 12:26:43 +0530

    Update in Middlewares

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,14 +1,14 @@
 
-import { Request, Response , NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken"
 
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
 
-interface JwtPayload{
-    id : string;
+interface JwtPayload {
+    id: string;
 }
 
-export const verifyToken = (req : Request , res : Response , next : NextFunction) =>{
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const token = req.header("Authorization")
     //console.log(token);
 
@@ -16,13 +16,13 @@ export const verifyToken = (req : Request , res : Response , next : NextFunction
         return res.status(401).json({ error: "Access denied. No token provided." });
     }
 
-    try{
-        const decode = jwt.verify(token,JWT_SECRET) as JwtPayload;
-        req.user = decode;
-        next()
-    }catch(error){
+    try {
+        const decode = jwt.verify(token, JWT_SECRET) as JwtPayload;
+        req.user = { id: decode.id }; // Add the decoded JWT payload to req.user
+        next();
+    } catch (error) {
         return res.status(400).json({
-            error : "Invalid Token"
+            error: "Invalid Token"
         })
     }
 }
