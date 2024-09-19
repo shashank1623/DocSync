f62b160b1228d20d615726889b722b17eab4b937
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-19 12:27:21 +0530

    Types update

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -4,7 +4,7 @@ import { JwtPayload } from "jsonwebtoken";
 declare global {
   namespace Express {
     interface Request {
-      user?: JwtPayload; // Make sure JwtPayload is imported or defined if needed
+      user?: JwtPayload | { id : string}; // Make sure JwtPayload is imported or defined if needed
     }
   }
 }
