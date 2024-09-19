3c29da6a156fcf4393dbd266f4bc1cfda9fa491f
shashank1623 <shashankbhardwaj2030@gmail.com>
2024-09-19 14:03:39 +0530

    Url issue fixed

--- frontend/src/config.tsx
+++ frontend/src/config.tsx
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://docsync-njzk.onrender.com/";
+export const BACKEND_URL = "https://docsync-njzk.onrender.com";
 export const FRONTED_URL = "https://docsync.shashankbhardwaj.me"
